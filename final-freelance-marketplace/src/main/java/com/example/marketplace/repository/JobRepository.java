afe8dcd4c2547a379e26dc73ee9dd37ba96e81aa
MIkita Dubrovin <115701206+jowokji@users.noreply.github.com>
2025-06-08 22:47:05 +0300

    Delete final-freelance-marketplace directory

--- final-freelance-marketplace/src/main/java/com/example/marketplace/repository/JobRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.marketplace.repository;
-
-import com.example.marketplace.model.Job;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JobRepository extends JpaRepository<Job, Integer> {
-}
